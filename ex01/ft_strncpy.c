0c212684d38a88c49e376991be1f3a446316270b
tugbayangoz04 <143502388+tugbayangoz04@users.noreply.github.com>
2024-08-26 12:16:21 +0300

    Add files via upload

---
+++ ex01/ft_strncpy.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncopy.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyangoz <tyangoz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/24 14:24:43 by tyangoz           #+#    #+#             */
+/*   Updated: 2024/08/24 16:02:51 by tyangoz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <unistd.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i = 0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
---
+++ ex09/ft_strcapitalize.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcapitalize.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tyangoz <tyangoz@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/25 13:14:27 by tyangoz           #+#    #+#             */
+/*   Updated: 2024/08/25 15:47:13 by tyangoz          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 1;
+	ft_strlowcase(str);
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 'a' && str[i] <= 'z')
+		{
+			if (j == 1)
+				str[i] -= 32;
+			j = 0;
+		}
+		else if (str[i] >= '0' && str[i] <= '9')
+			j = 0;
+		else
+			j = 1;
+		i++;
+	}
+	return (str);
+}
